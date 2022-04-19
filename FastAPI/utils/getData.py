c46fed1019270e086d565a080a36c4c42585e667
Kun <ah09program@gmail.com>
2022-04-19 19:25:10 +0700

    update

--- FastAPI/utils/getData.py
+++ FastAPI/utils/getData.py
@@ -1,4 +1,6 @@
 import json
+from turtle import position
+import numpy as np
 from pymongo import MongoClient
 from bson.objectid import ObjectId
 import re
@@ -16,6 +18,9 @@ code = database["code"]
 table = database["table"]
 keyphrase = database["keyphrase"]
 cau_hoi = database["cau hoi"]
+
+def findOccurrences(str_, substr):
+    return [m.start() for m in re.finditer(substr, str_)]
 
 def getChapter():
     list_data = []
@@ -44,59 +49,173 @@ def getContentById(strId):
         "Mo_ta" : content["Mo_ta"],
         "Phan_muc" : []
     }
-    for chapter in chuong.find({}):
-        if chapter["_id"] == content["id_chuong"]:
-            data["ten_chuong"] = chapter["ten_chuong"]
-    for index in phan_muc.find({}):
-        if index["id_noi_dung"] == content["_id"]:
-            data_index =  {
-                "ten_muc" : index["ten_muc"],
-                "noi_dung" : index["noi_dung"],
-                "media" : [],
-                "code" : [],
-                "table" : []
-            }
-            for m in media.find({}):
-                if m["id_muc"] == index["_id"]:
-                    data_index["media"].append({
-                        "Link" : m["Link"],
-                        "Loai" : m["Loai"]
-                    })
-            for c in code.find({}):
-                if c["id_muc"] == index["_id"]:
-                    data_index["code"].append(c["Code"])
-            for t in table.find({}):
-                if t["id_muc"] == index["_id"]:
-                    data_index["table"].append(t["table"])
-            data["Phan_muc"].append(data_index)
 
+    data["ten_chuong"] = chuong.find({"_id" : ObjectId(content["id_chuong"])})[0]["ten_chuong"]
+    for index in phan_muc.find({"id_noi_dung" : ObjectId(content["_id"])}):
+        position = []
+        position.append({
+            "pos" : 0,
+            "type" : ""
+        })
+
+        data_index =  {
+            "ten_muc" : index["ten_muc"],
+            "noi_dung" : [],
+            "bo_sung" : []
+        }
+
+        for type in ["code","img","href","table"]:
+            for pos in findOccurrences(index["noi_dung"], f"\|{type}\|"):
+                position.append({
+                    "pos" : pos,
+                    "type" : f"|{type}|\n"
+                })
+
+        position.append({
+            "pos" : len(index["noi_dung"]),
+            "type" : ""
+        })
+
+        #print(index["noi_dung"])
+        position = sorted(position, key=lambda d: d['pos']) 
+        # for pos in position:
+        #     print(pos)
+        
+        list_img = []
+        list_code = []
+        list_table = []
+        list_href = []
+
+        for m in media.find({"id_muc" : ObjectId(str(index["_id"]))}):
+            if m["Loai"] == "img":
+                list_img.append(m["Link"])
+            else:
+                list_href.append(m["Link"])
+        for c in code.find({"id_muc" : ObjectId(str(index["_id"]))}):
+            list_code.append(c["Code"])
+        for t in table.find({"id_muc" : ObjectId(str(index["_id"]))}):
+            list_table.append(t["table"])
+
+        for pos in position:
+            if pos["type"] == "|code|\n":
+                data_index["bo_sung"].append({
+                    "noi_dung" : list_code[0][1:],
+                    "Loai" : "code"
+                })
+                list_code = np.delete(list_code, 0, None)
+            if pos["type"] == "|img|\n":
+                data_index["bo_sung"].append({
+                    "noi_dung" : list_img[0],
+                    "Loai" : "img"
+                })
+                list_img = np.delete(list_img, 0, None)
+            if pos["type"] == "|href|\n":
+                data_index["bo_sung"].append({
+                    "noi_dung" : list_href[0],
+                    "Loai" : "href"
+                })
+                list_href = np.delete(list_href, 0, None)
+            if pos["type"] == "|table|\n":
+                data_index["bo_sung"].append({
+                    "noi_dung" : list_table[0],
+                    "Loai" : "table"
+                })
+                list_table = np.delete(list_table, 0, None)
+
+
+        while len(position) != 1:
+            data_index["noi_dung"].append(index["noi_dung"][position[0]["pos"]+len(position[0]["type"]):position[1]["pos"]])
+            position = np.delete(position, 0, None)
+        
+        data["Phan_muc"].append(data_index)
     return data
+
+        
+
+# rgx = re.compile(f'.*Tham chiếu.*', re.IGNORECASE)
+# content = noi_dung.find({"ten_noi_dung" : rgx})[0]
+# data = getContentById(str(content["_id"]))
+
+
+    # return data
 
 def getIndexById(strId):
     index = phan_muc.find({"_id": ObjectId(strId)})[0]
+    position = []
+    position.append({
+        "pos" : 0,
+        "type" : ""
+    })
+
     data_index =  {
-        "ten_noi_dung" : "",
         "ten_muc" : index["ten_muc"],
-        "noi_dung" : index["noi_dung"],
-        "media" : [],
-        "code" : [],
-        "table" : []
+        "noi_dung" : [],
+        "bo_sung" : []
     }
-    for nd in noi_dung.find({}):
-        if nd["_id"] == index["id_noi_dung"]:
-            data_index["ten_noi_dung"] = nd["ten_noi_dung"]
-    for m in media.find({}):
-        if m["id_muc"] == index["_id"]:
-            data_index["media"].append({
-                "Link" : m["Link"],
-                "Loai" : m["Loai"]
+
+    for type in ["code","img","href","table"]:
+        for pos in findOccurrences(index["noi_dung"], f"\|{type}\|"):
+            position.append({
+                "pos" : pos,
+                "type" : f"|{type}|\n"
             })
-    for c in code.find({}):
-        if c["id_muc"] == index["_id"]:
-            data_index["code"].append({"" : c["Code"]})
-    for t in table.find({}):
-        if t["id_muc"] == index["_id"]:
-            data_index["table"].append({"" : t["table"]})
+
+    position.append({
+        "pos" : len(index["noi_dung"]),
+        "type" : ""
+    })
+
+    #print(index["noi_dung"])
+    position = sorted(position, key=lambda d: d['pos']) 
+    # for pos in position:
+    #     print(pos)
+    
+    list_img = []
+    list_code = []
+    list_table = []
+    list_href = []
+
+    for m in media.find({"id_muc" : ObjectId(str(index["_id"]))}):
+        if m["Loai"] == "img":
+            list_img.append(m["Link"])
+        else:
+            list_href.append(m["Link"])
+    for c in code.find({"id_muc" : ObjectId(str(index["_id"]))}):
+        list_code.append(c["Code"])
+    for t in table.find({"id_muc" : ObjectId(str(index["_id"]))}):
+        list_table.append(t["table"])
+
+    for pos in position:
+        if pos["type"] == "|code|\n":
+            data_index["bo_sung"].append({
+                "noi_dung" : list_code[0][2:],
+                "Loai" : "code"
+            })
+            list_code = np.delete(list_code, 0, None)
+        if pos["type"] == "|img|\n":
+            data_index["bo_sung"].append({
+                "noi_dung" : list_img[0],
+                "Loai" : "img"
+            })
+            list_img = np.delete(list_img, 0, None)
+        if pos["type"] == "|href|\n":
+            data_index["bo_sung"].append({
+                "noi_dung" : list_href[0],
+                "Loai" : "href"
+            })
+            list_href = np.delete(list_href, 0, None)
+        if pos["type"] == "|table|\n":
+            data_index["bo_sung"].append({
+                "noi_dung" : list_table[0],
+                "Loai" : "table"
+            })
+            list_table = np.delete(list_table, 0, None)
+
+
+    while len(position) != 1:
+        data_index["noi_dung"].append(index["noi_dung"][position[0]["pos"]+len(position[0]["type"]):position[1]["pos"]])
+        position = np.delete(position, 0, None)
+        
     return data_index
 
 def findWithKeyphrase(keyph):
@@ -119,13 +238,13 @@ def getQuestionRandom(number_of_question):
     list_data = [list_question[idx] for idx in random.sample(range(len(list_question)), number_of_question)]
     for data in list_data:
         data["_id"] = str(data["_id"])
-        # data.pop('Dap_an', None)
+        data.pop('Dap_an', None)
         data.pop("id_muc", None)
     return list_data
 
-# rgx = re.compile(f'.*Tham chiếu.*', re.IGNORECASE)
-# content = noi_dung.find({"ten_noi_dung" : rgx})[0]
+with open('data_question.json', 'w', encoding='utf-8') as f:
+    json.dump(getQuestionRandom(20), f, ensure_ascii=False)
 
-data = findWithKeyphrase("const")
-with open('data_lesson.json', 'w', encoding='utf-8') as f:
-    json.dump(data, f, ensure_ascii=False)
+
+
+# data = findWithKeyphrase("const")
